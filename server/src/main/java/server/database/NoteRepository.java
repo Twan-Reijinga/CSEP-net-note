06f13c8fbec5f57226a277d7aa8267ad07487a7e
Petar Iliev <p.g.iliev@student.tudelft.nl>
2024-11-27 08:34:24 +0100

    Revert "Merge branch 'basic-functionality' into 'main'"
    This reverts merge request !3

--- server/src/main/java/server/database/NoteRepository.java
+++
@@ -1,9 +0,0 @@
-package server.database;
-
-import commons.Note;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NoteRepository extends JpaRepository<Note, Long> {
-}
