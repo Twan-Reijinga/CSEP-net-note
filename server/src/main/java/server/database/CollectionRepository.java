5b58bbf52448290a310bd2ba79c8274d4c83d5ed
OLEH Dzoba <o.v.dzoba@student.tudelft.nl>
2024-11-27 11:39:22 +0100

    Reapply "Merge branch 'basic-functionality' into 'main'"
    This reverts commit 06f13c8fbec5f57226a277d7aa8267ad07487a7e.

---
+++ server/src/main/java/server/database/CollectionRepository.java
@@ -0,0 +1,9 @@
+package server.database;
+
+import commons.Collection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CollectionRepository extends JpaRepository<Collection, String> {
+}
