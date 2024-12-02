8696078c64e08c729491d1203a32245fa2d7eba2
Pepijn Hulsbosch <p.hulsbosch@student.tudelft.nl>
2024-12-02 12:35:50 +0100

    Add and deleting notes

--- server/src/main/java/server/api/NoteController.java
+++ server/src/main/java/server/api/NoteController.java
@@ -5,16 +5,13 @@ import commons.Note;
 import commons.NoteMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import server.database.NoteRepository;
 
 @RestController
 @RequestMapping("/api/notes")
 public class NoteController {
-    private NoteRepository noteRepository;
+    private final NoteRepository noteRepository;
 
     @Autowired
     public NoteController(NoteRepository noteRepository) {
@@ -54,5 +51,40 @@ public class NoteController {
         List<NoteMapper> mapped = notes.stream().map(x -> new NoteMapper(x.id, x.title)).toList();
 
         return mapped;
+    }
+
+    /**
+     * Stores a new note in the database, with the provided arguments.
+     * Note can not have empty values, and has to have a collection.
+     * The method needs to be addressed through making a client.Post.
+     * <p>
+     * {@code @Param} note  the entity that needs to be stored in database
+     * {@code @Return} a http Response, either bad-build or good
+     */
+    @PostMapping("")
+    public ResponseEntity<Note> add(@RequestBody Note note) {
+        if (note.collection == null || note.id <= 0 || note.title == null || noteRepository.existsById(note.id)) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        Note saved = noteRepository.save(note);
+        return ResponseEntity.ok(saved);
+    }
+
+    /**
+     * Removes the note of the provided ID from database
+     * id has to match with a note in the database.
+     * <p>
+     * @param id  the identifier of a note
+     * @return a http Response, either a bad build or a valid one
+     */
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Note> delete(@PathVariable("id") long id) {
+        if (id < 0 || !noteRepository.existsById(id)){
+            return ResponseEntity.badRequest().build();
+        }
+        Note removed = noteRepository.findById(id).get();
+        noteRepository.deleteById(id);
+        return ResponseEntity.ok(removed);
     }
 }
