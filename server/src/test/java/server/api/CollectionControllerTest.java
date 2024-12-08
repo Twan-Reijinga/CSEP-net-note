9deff6d480100753a0052994c1c7fbefcc23fc52
Petar Iliev <P.G.Iliev@student.tudelft.nl>
2024-12-08 18:42:33 +0100

    Checkstyle error fix

--- server/src/test/java/server/api/CollectionControllerTest.java
+++ server/src/test/java/server/api/CollectionControllerTest.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+//import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 public class CollectionControllerTest {
 
