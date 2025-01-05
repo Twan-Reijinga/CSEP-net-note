8abd95b72143fc8f1fa0edb106dd86ab9e0ffb12
Pepijn Hulsbosch <p.hulsbosch@student.tudelft.nl>
2025-01-05 19:05:59 +0100

    removed all unused imports

--- server/src/main/java/server/database/EmbeddedFileRepository.java
+++ server/src/main/java/server/database/EmbeddedFileRepository.java
@@ -1,15 +1,12 @@
 package server.database;
 
-import commons.Collection;
 import commons.EmbeddedFile;
-import commons.Note;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface EmbeddedFileRepository extends JpaRepository<EmbeddedFile, Long> {
