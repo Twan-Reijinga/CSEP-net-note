0aedc135e3ee8c3cf7b811d6ae709cc550b290d0
Twan-Reijinga <reijinga@tuta.io>
2025-01-17 11:23:40 +0100

    inprove LanguagePreferenceTest with before and after

--- client/src/test/java/client/config/LanguagePreferenceTest.java
+++ client/src/test/java/client/config/LanguagePreferenceTest.java
@@ -2,6 +2,9 @@ package client.config;
 
 import client.utils.Language;
 import com.google.inject.Key;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.prefs.Preferences;
@@ -12,85 +15,52 @@ class LanguagePreferenceTest {
 
     private static final String LANGUAGE_KEY = "language_option";
     private static final Preferences PREFERENCES = Preferences.userNodeForPackage(LanguagePreference.class);
+    private String language;
 
+    @BeforeEach
+    void setUp() {
+        language = PREFERENCES.get(LANGUAGE_KEY, "English");
+    }
+
+    @AfterEach
+    void tearDown() {
+        PREFERENCES.put(LANGUAGE_KEY, language);
+
+    }
 
     @Test
     void saveLanguageSpanish() {
-        // store
-        String lang = PREFERENCES.get(LANGUAGE_KEY, "English");
-
-        // check
         LanguagePreference.saveLanguage(Language.ES);
         assertEquals("Spanish", PREFERENCES.get(LANGUAGE_KEY, "English"));
-
-        // cleanup
-        PREFERENCES.put(LANGUAGE_KEY, lang);
     }
 
     @Test
     void saveLanguageDutch() {
-        // store
-        String lang = PREFERENCES.get(LANGUAGE_KEY, "English");
-
-        // check
         LanguagePreference.saveLanguage(Language.NL);
         assertEquals("Dutch", PREFERENCES.get(LANGUAGE_KEY, "English"));
-
-        // cleanup
-        PREFERENCES.put(LANGUAGE_KEY, lang);
     }
 
     @Test
     void saveLanguageEnglish() {
-        // store
-        String lang = PREFERENCES.get(LANGUAGE_KEY, "English");
-
-        // check
         LanguagePreference.saveLanguage(Language.EN);
         assertEquals("English", PREFERENCES.get(LANGUAGE_KEY, "English"));
-
-        // cleanup
-        PREFERENCES.put(LANGUAGE_KEY, lang);
     }
 
     @Test
     void getLanguageSpanish() {
-        // setup
-        String lang = PREFERENCES.get(LANGUAGE_KEY, "English");
         LanguagePreference.saveLanguage(Language.ES);
-
-
-        // check
         assertEquals(Language.ES, LanguagePreference.getLanguage());
-
-        // cleanup
-        PREFERENCES.put(LANGUAGE_KEY, lang);
     }
 
     @Test
     void getLanguageDutch() {
-        // setup
-        String lang = PREFERENCES.get(LANGUAGE_KEY, "English");
         LanguagePreference.saveLanguage(Language.NL);
-
-
-        // check
         assertEquals(Language.NL, LanguagePreference.getLanguage());
-
-        // cleanup
-        PREFERENCES.put(LANGUAGE_KEY, lang);
     }
 
     @Test
     void getLanguageEnglish() {
-        // store
-        String lang = PREFERENCES.get(LANGUAGE_KEY, "English");
-
-        // check
         LanguagePreference.saveLanguage(Language.EN);
         assertEquals(Language.EN, LanguagePreference.getLanguage());
-
-        // cleanup
-        PREFERENCES.put(LANGUAGE_KEY, lang);
     }
 }
