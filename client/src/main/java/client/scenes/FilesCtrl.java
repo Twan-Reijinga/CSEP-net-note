046b6246ec0fabf1de24c091d7aa0ad8b01a0e20
Pepijn Hulsbosch <p.hulsbosch@student.tudelft.nl>
2025-01-19 22:59:00 +0100

    checkstyle fixes

--- client/src/main/java/client/scenes/FilesCtrl.java
+++ client/src/main/java/client/scenes/FilesCtrl.java
@@ -10,8 +10,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyCode;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
