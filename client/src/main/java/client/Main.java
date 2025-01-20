fec12c15a13ee2e95021eb994d528e622ef52a27
Oleh Dzoba <82672968+olehdzoba@users.noreply.github.com>
2025-01-20 16:08:46 +0100

    Removed lang preferences in favour of a single Config entity

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -17,9 +17,9 @@ package client;
 
 import static com.google.inject.Guice.createInjector;
 
+import client.config.Config;
 import client.scenes.SidebarCtrl;
 import client.utils.Language;
-import client.config.LanguagePreference;
 import com.google.inject.Injector;
 import client.scenes.NoteEditorCtrl;
 import client.scenes.MainCtrl;
@@ -37,6 +37,7 @@ public class Main extends Application {
 
 	private static final Injector INJECTOR = createInjector(new GuiceModule());
 	private static final LoaderFXML FXML = INJECTOR.getInstance(LoaderFXML.class);
+	private static final Config CONFIG = INJECTOR.getInstance(Config.class);
 
 	public static void main(String[] args) {
 		launch(args);
@@ -51,7 +52,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		primaryStage = stage;
-		Language language = LanguagePreference.getLanguage();
+		Language language = CONFIG.getLanguage();
 		loadApplication(language);
 	}
 
@@ -95,8 +96,8 @@ public class Main extends Application {
 	 * @param language the language configuration to be applied to the application
 	 */
 	public static void switchLanguage(Language language) {
-		if (language == LanguagePreference.getLanguage()) return;
-		LanguagePreference.saveLanguage(language);
+		if (language == CONFIG.getLanguage()) return;
+		CONFIG.setLanguage(language);
 		loadApplication(language);
 	}
 }
--- client/src/main/java/client/config/Config.java
+++ client/src/main/java/client/config/Config.java
@@ -3,6 +3,7 @@ package client.config;
 import java.util.*;
 import java.io.*;
 
+import client.utils.Language;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import commons.Collection;
 
@@ -10,7 +11,10 @@ public class Config {
 
     private static final String CONFIG_LOCATION = "user-config.json";
 
-    private UUID defaultCollectionId;
+    // Default collection ID is supposed to be null at initial launch
+    private UUID defaultCollectionId = null;
+    // Language defaults to English
+    private Language language = Language.EN;
 
     public Config() {
         // for object mapper
@@ -20,9 +24,16 @@ public class Config {
         ObjectMapper objectMapper = new ObjectMapper();
         File configFile = new File(CONFIG_LOCATION);
         try {
-            return objectMapper.readValue(configFile, Config.class);
+            Config config = objectMapper.readValue(configFile, Config.class);
+
+            // Language not allowed to be null
+            if (config.language == null) {
+                config.language = Language.EN;
+            }
+
+            return config;
         } catch (IOException e) {
-            System.out.println("Config not found. Creating a default config...");
+            System.out.println("Config is not available. Creating a default config...");
             System.out.println(e.getMessage());
 
             return new Config();
@@ -40,10 +51,35 @@ public class Config {
         }
     }
 
+    /**
+     * Restore language from storage to use in app when re-opened.
+     * @return The stored language or the default english.
+     */
+    public Language getLanguage() {
+        return language;
+    }
+
+    /**
+     * Save language locally to restore preference when app is opened again.
+     * @param language The language to save.
+     */
+    public void setLanguage(Language language) {
+        this.language = language;
+        save();
+    }
+
+    /**
+     * Get the default collection ID from the local config file
+     * @return a UUID of default collection
+     */
     public UUID getDefaultCollectionId() {
         return defaultCollectionId;
     }
 
+    /**
+     * Set a new ID for default collection and save to the local config file
+     * @param defaultCollectionId an ID of a new default collection
+     */
     public void setDefaultCollectionId(UUID defaultCollectionId) {
         this.defaultCollectionId = defaultCollectionId;
         save();
